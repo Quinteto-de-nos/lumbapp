8402a7aec27ca09de2c7717d0f071813818edd72
Juan <juan.elca@gmail.com>
2020-07-10 00:21:47 -0300

    arduino read conector

---
+++ LumbApp/Conectores/ConectorSI/conector.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO.Ports;
+using System.Text;
+
+class PortDataReceived
+{
+   // RegistroEstado registroEstado;
+
+    public static void Main()
+    {
+        SerialPort mySerialPort = new SerialPort("COM5");
+
+
+        mySerialPort.BaudRate = 9600;
+        mySerialPort.Parity = Parity.None;
+        mySerialPort.StopBits = StopBits.One;
+        mySerialPort.DataBits = 8;
+        mySerialPort.Handshake = Handshake.None;
+        mySerialPort.RtsEnable = true;
+
+        mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+        mySerialPort.Open();
+
+        Console.WriteLine("Press any key to continue...");
+        Console.WriteLine();
+        Console.ReadKey();
+        mySerialPort.Close();
+    }
+
+    private static void DataReceivedHandler(
+                        object sender,
+                        SerialDataReceivedEventArgs e)
+    {
+        
+        SerialPort sp = (SerialPort)sender;
+        //string indata = sp.ReadExisting();
+        //string indata = sp.ReadLine();
+       // int data = sp.ReadByte();
+        //Console.Write(indata);
+        string datos = sp.ReadTo("$");
+
+        //Console.WriteLine(datos);
+        Console.WriteLine(datos[6]);
+        //registroEstado = new RegistroEstado();
+
+
+
+
+
+
+    }
+}
