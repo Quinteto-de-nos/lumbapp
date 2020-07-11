e11ca150218cfba83ccb024aea88961ce7649763
leyluchy <valeria.silvestri@vdfactory.com>
2020-07-11 14:48:20 -0300

    Arranco experto ZE

---
+++ LumbApp/Expertos/ExpertoZE/ExpertoZE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LumbApp.Expertos.ExpertoZE
+{
+    class ExpertoZE
+    {
+        public ExpertoZE() { }
+
+        public bool Inicializar() { return false; }
+        public bool IniciarSimulacion() { return false;  }
+        public void TerminarSimulacion() { }
+        public void GetInforme() { }
+        public void Finalizar() { }
+    }
+}
