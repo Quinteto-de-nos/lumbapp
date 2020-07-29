86efd07c04471b634d60527ba40b3a7aa5526b53
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2020-07-29 02:25:20 -0300

    added new methods to orchestrator

---
+++ LumbApp/Expertos/ExpertoZE/InformeZE.cs
@@ -0,0 +1,16 @@
+﻿namespace LumbApp.Expertos.ExpertoZE
+{
+    public class InformeZE
+    {
+        int _cantContaminadasTotal;
+        int _cantContaminadasI;
+        int _cantContaminadasD;
+
+        public InformeZE(int cantContaminadasTotal, int cantContaminadasI, int cantContaminadasD)
+        {
+            _cantContaminadasTotal = cantContaminadasTotal;
+            _cantContaminadasI = cantContaminadasI;
+            _cantContaminadasD = cantContaminadasD;
+        }
+    }
+}
