ce820f2c7f51c8b1a122c6dc04d125993e3a9496
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2020-09-06 17:16:55 -0300

    added interface

---
+++ LumbApp/Conectores/ConectorFS/ConectorFS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LumbApp.Conectores.ConectorFS
+{
+    class ConectorFS : IConectorFS
+    {
+        public bool GuardarObjectoComoArchivoDeTexto<T>(string path, T objectToBeSavedAsJson)
+        {
+            String jsonStringToBeSaved = JsonSerializer.Serialize<T>(objectToBeSavedAsJson);
+        }
+
+        public T LevantarArchivoDeTextoComoObjeto<T>(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
