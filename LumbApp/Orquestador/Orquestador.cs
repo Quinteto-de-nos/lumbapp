a7fc781ed5df0bd2c873cf5d5b1485e439569429
Carla <carla-hamm@hotmail.es>
2020-09-06 21:42:48 -0300

    feature/InicializarOrquestador

--- LumbApp/Orquestador/Orquestador.cs
+++ LumbApp/Orquestador/Orquestador.cs
@@ -74,22 +74,22 @@ namespace LumbApp.Orquestador
 			try {
 				//INICIALIZAR EXPERTO ZE
 				expertoZE.CambioZE += CambioZE; //suscripción al evento CambioZE
-				if (!expertoZE.Inicializar()) {
-					expertoZE.CambioZE -= CambioZE; //Ver si hay que desuscribir en caso de error
+				if (!expertoZE.Inicializar())
 					throw new Exception("No se pudo detectar correctamente la kinect.");
-				}
 
 				//INICIALIZAR EXPERTO SI
 				expertoSI.CambioSI += CambioSI; //suscripción al evento CambioSI
-				if (!expertoSI.Inicializar()) {
-					expertoSI.CambioSI -= CambioSI;
+				if (!expertoSI.Inicializar())
 					throw new Exception("No se pudo detectar correctamente los sensores internos.");
-				}
 
 				//Mostrar pantalla de ingreso de datos
 				GUIController.SolicitarDatosPracticante();
 
 			} catch (Exception ex) {
+				expertoZE.CambioZE -= CambioZE;
+				if(ex.Message.Contains("sensores"))
+					expertoSI.CambioSI -= CambioSI;
+
 				GUIController.MostrarErrorDeConexion(ex.Message);
 			}
 			
