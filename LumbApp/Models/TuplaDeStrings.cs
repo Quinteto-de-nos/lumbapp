52c1f3eebbc0af6ccfbd853edab19c6d5830bfcc
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2020-09-30 20:43:58 -0300

    added list of informe attributes

---
+++ LumbApp/Models/TuplaDeStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumbApp.Models
+{
+    public class TuplaDeStrings
+    {
+        string Clave { get; }
+        string Valor { get; }
+
+        public TuplaDeStrings (string clave, string valor)
+        {
+            this.Clave = clave;
+            this.Valor = valor;
+        }
+    }
+}
