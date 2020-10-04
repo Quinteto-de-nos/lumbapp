3e42dcbc944a762f160b66749a3bb797588e8d7b
leyluchy <valeria.silvestri@vdfactory.com>
2020-10-04 14:49:16 -0300

    Corrijo tests

--- UnitTestLumbapp/UnitTestOrquestador.cs
+++ UnitTestLumbapp/UnitTestOrquestador.cs
@@ -77,9 +77,18 @@ namespace UnitTestLumbapp {
             orq.SetExpertoZE(expZE.Object);
 
             orq.Inicializar();
-            gui.Verify(x => x.SolicitarDatosPracticante(It.IsAny<string>()), Times.Once);
-            orq.SetDatosDeSimulacion(new Mock<LumbApp.Models.DatosPracticante>().Object, LumbApp.Enums.ModoSimulacion.ModoEvaluacion);
+
+            var datos = new DatosPracticante()
+            {
+                Dni = 99999999,
+                Nombre = "Maximo",
+                Apellido = "Cozzetti",
+                Email = "mcozzetti@alumno.unlam.edu.ar",
+                FolderPath = "C:\\Cozzetti\\Desktop"
+            };
+            orq.SetDatosDeSimulacion(datos, LumbApp.Enums.ModoSimulacion.ModoEvaluacion);
             orq.IniciarSimulacion();
+
             expSI.Verify(x => x.IniciarSimulacion(), Times.Once);
             expZE.Verify(x => x.IniciarSimulacion(It.IsAny<IVideo>()), Times.Once);
             gui.Verify(x => x.IniciarSimulacionModoEvaluacion(), Times.Once);
@@ -100,9 +109,17 @@ namespace UnitTestLumbapp {
             orq.SetExpertoZE(expZE.Object);
 
             orq.Inicializar();
-            gui.Verify(x => x.SolicitarDatosPracticante(It.IsAny<string>()), Times.Once);
-            orq.SetDatosDeSimulacion(new Mock<LumbApp.Models.DatosPracticante>().Object, LumbApp.Enums.ModoSimulacion.ModoGuiado);
+            var datos = new DatosPracticante()
+            {
+                Dni = 99999999,
+                Nombre = "Maximo",
+                Apellido = "Cozzetti",
+                Email = "mcozzetti@alumno.unlam.edu.ar",
+                FolderPath = "C:\\Cozzetti\\Desktop"
+            };
+            orq.SetDatosDeSimulacion(datos, LumbApp.Enums.ModoSimulacion.ModoGuiado);
             orq.IniciarSimulacion();
+
             expSI.Verify(x => x.IniciarSimulacion(), Times.Once);
             expZE.Verify(x => x.IniciarSimulacion(It.IsAny<IVideo>()), Times.Once);
             gui.Verify(x => x.IniciarSimulacionModoGuiado(), Times.Once);
@@ -123,10 +140,18 @@ namespace UnitTestLumbapp {
             orq.SetExpertoZE(expZE.Object);
 
             orq.Inicializar();
-            gui.Verify(x => x.SolicitarDatosPracticante(It.IsAny<string>()), Times.Once);
 
-            orq.SetDatosDeSimulacion(new Mock<LumbApp.Models.DatosPracticante>().Object, LumbApp.Enums.ModoSimulacion.ModoEvaluacion);
+            var datos = new DatosPracticante()
+            {
+                Dni = 99999999,
+                Nombre = "Maximo",
+                Apellido = "Cozzetti",
+                Email = "mcozzetti@alumno.unlam.edu.ar",
+                FolderPath = "C:\\Cozzetti\\Desktop"
+            };
+            orq.SetDatosDeSimulacion(datos, LumbApp.Enums.ModoSimulacion.ModoEvaluacion);
             orq.IniciarSimulacion();
+
             expSI.Verify(x => x.IniciarSimulacion(), Times.Once);
             expZE.Verify(x => x.IniciarSimulacion(It.IsAny<IVideo>()), Times.Once);
             gui.Verify(x => x.IniciarSimulacionModoEvaluacion(), Times.Once);
@@ -203,30 +228,35 @@ namespace UnitTestLumbapp {
             expZE.Setup(x => x.TerminarSimulacion()).Returns(informeZE);
 
             orq.TerminarSimulacion();
+
             var arg = new ArgumentCaptor<Informe>();
             gui.Verify(x => x.MostrarResultados(arg.Capture()), Times.Once);
             Informe informe = arg.Value;
             Assert.IsNotNull(informe);
+
             Assert.AreEqual(dp.Dni, informe.Dni);
             Assert.AreEqual(dp.Nombre, informe.Nombre);
             Assert.AreEqual(dp.Apellido, informe.Apellido);
             Assert.AreEqual(dp.FolderPath, informe.FolderPath);
-            Assert.AreEqual(informeZE.ManoDerecha, informe.DatosPractica["Contaminaciones Mano Derecha"]);
-            Assert.AreEqual(informeZE.ManoIzquierda, informe.DatosPractica["Contaminaciones Mano Izquierda"]);
-            Assert.AreEqual(informeZE.Zona, informe.DatosPractica["Contaminaciones Zona"]);
-            Assert.AreEqual(informeSI.TejidoAdiposo, informe.DatosPractica["Punciones Tejido Adiposo"]);
-            Assert.AreEqual(informeSI.L5, informe.DatosPractica["Roces L5"]);
-            Assert.AreEqual(informeSI.L4ArribaIzquierda, informe.DatosPractica["Roces L4 Arriba Izquierda"]);
-            Assert.AreEqual(informeSI.L4ArribaDerecha, informe.DatosPractica["Roces L4 Arriba Derecha"]);
-            Assert.AreEqual(informeSI.L4ArribaCentro, informe.DatosPractica["Roces L4 Arriba Centro"]);
-            Assert.AreEqual(informeSI.L4Abajo, informe.DatosPractica["Roces L4 Abajo"]);
-            Assert.AreEqual(informeSI.L3Arriba, informe.DatosPractica["Roces L3 Arriba"]);
-            Assert.AreEqual(informeSI.L3Abajo, informe.DatosPractica["Roces L3 Abajo"]);
-            Assert.AreEqual(informeSI.L2, informe.DatosPractica["Roces L2"]);
-            Assert.AreEqual(informeSI.Duramadre, informe.DatosPractica["Punciones Duramadre"]);
-            Assert.AreEqual(informeSI.CaminoIncorrecto, informe.DatosPractica["Camino Incorrecto"]);
-            Assert.AreEqual(informeSI.CaminoCorrecto, informe.DatosPractica["Camino Correcto"]);
-            Assert.AreNotEqual(TimeSpan.Zero, informe.DatosPractica["Tiempo Total"]);
+
+            Assert.AreEqual(informeZE.ManoDerecha.ToString(), informe.DatosPractica["Contaminaciones Mano Derecha"]);
+            Assert.AreEqual(informeZE.ManoIzquierda.ToString(), informe.DatosPractica["Contaminaciones Mano Izquierda"]);
+            Assert.AreEqual(informeZE.Zona.ToString(), informe.DatosPractica["Contaminaciones Zona"]);
+
+            Assert.AreEqual(informeSI.TejidoAdiposo.ToString(), informe.DatosPractica["Punciones Tejido Adiposo"]);
+            Assert.AreEqual(informeSI.L5.ToString(), informe.DatosPractica["Roces L5"]);
+            Assert.AreEqual(informeSI.L4ArribaIzquierda.ToString(), informe.DatosPractica["Roces L4 Arriba Izquierda"]);
+            Assert.AreEqual(informeSI.L4ArribaDerecha.ToString(), informe.DatosPractica["Roces L4 Arriba Derecha"]);
+            Assert.AreEqual(informeSI.L4ArribaCentro.ToString(), informe.DatosPractica["Roces L4 Arriba Centro"]);
+            Assert.AreEqual(informeSI.L4Abajo.ToString(), informe.DatosPractica["Roces L4 Abajo"]);
+            Assert.AreEqual(informeSI.L3Arriba.ToString(), informe.DatosPractica["Roces L3 Arriba"]);
+            Assert.AreEqual(informeSI.L3Abajo.ToString(), informe.DatosPractica["Roces L3 Abajo"]);
+            Assert.AreEqual(informeSI.L2.ToString(), informe.DatosPractica["Roces L2"]);
+            Assert.AreEqual(informeSI.Duramadre.ToString(), informe.DatosPractica["Punciones Duramadre"]);
+            Assert.AreEqual(informeSI.CaminoIncorrecto.ToString(), informe.DatosPractica["Camino Incorrecto"]);
+            Assert.AreEqual(informeSI.CaminoCorrecto.ToString(), informe.DatosPractica["Camino Correcto"]);
+
+            Assert.AreNotEqual(TimeSpan.Zero.ToString(), informe.DatosPractica["Tiempo Total"]);
             Assert.IsTrue(informe.PdfGenerado);
         }
 
