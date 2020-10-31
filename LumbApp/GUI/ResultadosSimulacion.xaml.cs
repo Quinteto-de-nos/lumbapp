143925d47b95bf02428537f7e9f74982653cd010
Lara Jazmin Cavicchioli <laracavic132@gmail.com>
2020-10-31 16:03:13 -0300

    agrego spinner cuando esta procesando resultados

--- LumbApp/GUI/ResultadosSimulacion.xaml.cs
+++ LumbApp/GUI/ResultadosSimulacion.xaml.cs
@@ -18,6 +18,19 @@ namespace LumbApp.GUI
         {
             InitializeComponent();
             _controller = gui;
+            DatosPracticaBorder.Visibility = Visibility.Hidden;
+            DatosPracticaLabel.Visibility = Visibility.Hidden;
+            DatosPracticanteBorder.Visibility = Visibility.Hidden;
+            DatosPracticanteLabel.Visibility = Visibility.Hidden;
+            SpinerIcon.Visibility = Visibility.Visible;
+            TextoEspera.Visibility = Visibility.Visible;
+            BorderEspera.Visibility = Visibility.Visible;
+            NombrePracticante.Content = "";
+            DniPracticante.Content = "";
+            ReporteItemTitulo1.Content = "";
+            ReporteItemTitulo2.Content = "";
+            ReporteItemValor1.Content = "";
+            ReporteItemValor2.Content = "";
         }
 
         private void NuevaSimulacion_Click(object sender, RoutedEventArgs e)
@@ -27,14 +40,22 @@ namespace LumbApp.GUI
 
         public void MostrarResultados(Informe informe)
         {
+            DatosPracticaBorder.Visibility = Visibility.Visible;
+            DatosPracticaLabel.Visibility = Visibility.Visible;
+            DatosPracticanteBorder.Visibility = Visibility.Visible;
+            DatosPracticanteLabel.Visibility = Visibility.Visible;
+            SpinerIcon.Visibility = Visibility.Hidden;
+            TextoEspera.Visibility = Visibility.Hidden;
+            BorderEspera.Visibility = Visibility.Hidden;
+
             NombrePracticante.Content =
                 String.Format(
                     "{0}, {1}" + Environment.NewLine +
-                    "Resultados de la práctica en {2}", informe.Apellido, informe.Nombre,informe.FolderPath);
+                    "Resultados de la práctica en {2}", informe.Apellido, informe.Nombre, informe.FolderPath);
 
             DniPracticante.Content = "DNI: " + informe.Dni.ToString();
 
-            int halfPoint = informe.DatosPractica.Count / 2;
+            var halfPoint = Math.Ceiling(informe.DatosPractica.Count / 2.0);
             int i = 1;
 
             foreach (DictionaryEntry dato in informe.DatosPractica)
@@ -50,6 +71,11 @@ namespace LumbApp.GUI
                     ReporteItemValor2.Content += String.Format("{0}" + Environment.NewLine, dato.Value);
                 }
                 i++;
+            }
+            if (informe.DatosPractica.Count % 2 != 0)
+            {
+                ReporteItemTitulo2.Content += "  " + Environment.NewLine;
+                ReporteItemValor2.Content += "  " + Environment.NewLine;
             }
         }
 
